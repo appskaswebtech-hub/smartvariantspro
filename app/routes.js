f7744b569d030172d7c9c9b7c56381a3480f6c94
Avneet Singh <avneet.kaswebtech@gmail.com>
2026-09-08 14:32:10 +0530

    variant ui improved

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,8 @@
 import { flatRoutes } from "@react-router/fs-routes";
 
-export default flatRoutes();
+// Ignore editor-backup files (e.g. a stray `app.variants.jsx.save` left by
+// editing on the server) so they aren't treated as route modules and break
+// the build.
+export default flatRoutes({
+  ignoredRouteFiles: ["**/*.save", "**/*.bak", "**/*.orig", "**/*~"],
+});
